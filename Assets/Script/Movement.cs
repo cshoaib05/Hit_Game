f08a82eb09255a5d45d848a2efd1867b2636b073
cshoaib05 <choudharyshoaib05@gmail.com>
2019-08-12 18:56:42 +0530

    Aiming and velocity fixed

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -16,39 +16,8 @@ public class Movement : MonoBehaviour
         lr = GetComponent<LineRenderer>();
         lr.enabled = false;
         cam = Camera.main;
+        
     }
-
-
-
-
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            _velocity = new Vector3(10f, 0f, 0f);
-            rigid.AddForce(_velocity, ForceMode.VelocityChange);
-        }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.CompareTag("coll"))
-        {
-            ReflectProjectile(rigid,collision.GetContact(0).normal);   
-        }
-    }
-
-    private void ReflectProjectile(Rigidbody rb, Vector3 reflectVector)
-    {
-             reflectVector = reflectVector - new Vector3(0.1f,0f,0.1f);
-            _velocity = Vector3.Reflect(_velocity, reflectVector);
-            rigid.velocity = _velocity;
-    }
-
-
-
-
-
 
 
     private void FixedUpdate()
@@ -58,10 +27,10 @@ public class Movement : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-         
+
             lr.enabled = true;
             startpos = transform.position;
-            
+
             lr.SetPosition(0, startpos);
             lr.useWorldSpace = true;
         }
@@ -69,10 +38,10 @@ public class Movement : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
-            if (Physics.Raycast(ray, out hit,100))
+            if (Physics.Raycast(ray, out hit, 100))
             {
-                endpos = new Vector3(hit.point.x*-1f, transform.position.y, hit.point.z*-1f);
-               
+                endpos = new Vector3(hit.point.x * -1f, transform.position.y, hit.point.z * -1f);
+
                 lr.SetPosition(1, endpos);
 
                 lr.SetPosition(2, new Vector3(5, 0, 5));
@@ -83,7 +52,42 @@ public class Movement : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             lr.enabled = false;
+            
         }
+
+        if(rigid.IsSleeping())
+        {
+            transform.position = new Vector3(0, transform.position.y, 0);
+        }
+    }
+
+
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            _velocity = Vector3.Scale(endpos,new Vector3(10f,1f,10f));
+            rigid.AddForce(_velocity, ForceMode.VelocityChange);
+        }
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("coll"))
+        {
+            ReflectProjectile(collision.GetContact(0).normal);   
+        }
+    }
+
+
+    private void ReflectProjectile( Vector3 reflectVector)
+    {
+            // reflectVector = reflectVector - new Vector3(0.1f,0f,0.1f);
+            _velocity = Vector3.Reflect(_velocity, reflectVector);
+            
+            rigid.velocity = _velocity;
     }
 }
 
