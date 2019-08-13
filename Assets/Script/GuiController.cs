f3d3fe21f18f0edb12a22f57622fb6928d40e476
cshoaib05 <choudharyshoaib05@gmail.com>
2019-08-13 23:05:42 +0530

    Gui

---
+++ Assets/Script/GuiController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuiController : MonoBehaviour
+{
+    public static bool start;
+
+    [SerializeField] GameObject menupanel;
+    [SerializeField] GameObject pausebutton;
+    [SerializeField] GameObject storepanel;
+    [SerializeField] GameObject pausepanel;
+    [SerializeField] GameObject settingpanel;
+       
+    void Awake()
+    {
+        start = false; 
+    }
+
+    private void Update()
+    {
+        if(!start)
+        {
+            pausebutton.SetActive(false);
+        }
+        else
+        {
+            pausebutton.SetActive(true); 
+        }
+    }
+
+    public void Play()
+    {
+        start = true;
+        menupanel.SetActive(false);
+    }
+
+    public void Storepanel()
+    {
+        storepanel.SetActive(true);
+    }
+
+    public void Pausepanellshow()
+    {
+        Time.timeScale = 0;
+        pausepanel.SetActive(true);
+    }
+
+    public void settingpanelshow()
+    {
+        
+        settingpanel.SetActive(true);
+    }
+}
--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -9,9 +9,11 @@ public class Movement : MonoBehaviour
     Camera cam;
     LineRenderer lr;
     RaycastHit hit,objhit,scndhit;
+    public static bool isMoving;
 
     void Start()
     {
+        isMoving = false;
         rigid = GetComponent<Rigidbody>();
         lr = GetComponent<LineRenderer>();
         lr.enabled = false;
@@ -22,82 +24,93 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-       
-
-        if (Input.GetMouseButtonDown(0))
+        if(GuiController.start && !isMoving)
         {
-
-            lr.enabled = true;
-            startpos = transform.position;
-
-            lr.SetPosition(0, startpos);
-            lr.useWorldSpace = true;
-        }
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
 
-        if (Input.GetMouseButton(0))
-        {
-            if (Physics.Raycast(ray, out hit, 100))
+            if (Input.GetMouseButtonDown(0))
             {
-                endpos = new Vector3(hit.point.x * -1f, transform.position.y, hit.point.z * -1f);
-               
+
+                lr.enabled = true;
+                startpos = transform.position;
+
+                lr.SetPosition(0, startpos);
+                lr.useWorldSpace = true;
             }
+
+
+            if (Input.GetMouseButton(0))
+            {
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    endpos = new Vector3(hit.point.x * -1f, transform.position.y, hit.point.z * -1f);
+
+                }
+            }
+
+
+            if (Input.GetMouseButtonUp(0))
+            {
+               
+                lr.enabled = false;
+            }
+
+
         }
 
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            lr.enabled = false;
-        }
-
-    
     }
 
 
 
     private void Update()
     {
-        Ray objray = new Ray(transform.position, endpos);
-       
-        if (Physics.Raycast(objray, out objhit, 100))
+        if (GuiController.start && !isMoving)
         {
-           Vector3 scnd = new Vector3(objhit.point.x, transform.position.y, objhit.point.z);
-            lr.SetPosition(1, scnd);
+            Ray objray = new Ray(transform.position, endpos);
 
-            scndpos = Vector3.Reflect(endpos, objhit.normal);
-
-            Ray thrdray = new Ray(scnd, scndpos);
-
-            if (Physics.Raycast(thrdray,out scndhit,100))
+            if (Physics.Raycast(objray, out objhit, 100))
             {
-                Vector3 thrd = new Vector3(scndhit.point.x, transform.position.y, scndhit.point.z);
-                lr.SetPosition(2, thrd);
-                thrd = Vector3.Reflect(scndpos, scndhit.normal);
-                lr.SetPosition(3, thrd);
+                Vector3 scnd = new Vector3(objhit.point.x, transform.position.y, objhit.point.z);
+                lr.SetPosition(1, scnd);
+
+                scndpos = Vector3.Reflect(endpos, objhit.normal);
+
+                Ray thrdray = new Ray(scnd, scndpos);
+
+                if (Physics.Raycast(thrdray, out scndhit, 100))
+                {
+                    Vector3 thrd = new Vector3(scndhit.point.x, transform.position.y, scndhit.point.z);
+                    lr.SetPosition(2, thrd);
+                    thrd = Vector3.Reflect(scndpos, scndhit.normal);
+                    lr.SetPosition(3, thrd);
+                }
             }
+
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                _velocity = Vector3.Scale(endpos, new Vector3(10f, 1f, 10f));
+
+                rigid.AddForce(_velocity, ForceMode.VelocityChange);
+                isMoving = true;
+            }
+
+            
         }
-
-
-        if (Input.GetMouseButtonUp(0))
-        {
-            _velocity = Vector3.Scale(endpos,new Vector3(10f,1f,10f));
-
-            rigid.AddForce(_velocity, ForceMode.VelocityChange);
-        }
-
         StartCoroutine(checkmovement());
-    }
+        }
+
 
     IEnumerator checkmovement()
     {
         Vector3 pos = transform.position;
-        yield  return new WaitForSeconds(1);
-        if(pos == transform.position)
+        yield return new WaitForSeconds(1);
+        if (pos == transform.position)
         {
-            transform.position = new Vector3(0,transform.position.y, 0);
+            transform.position = new Vector3(0, transform.position.y, 0);
+            isMoving = false;
         }
-
     }
 }
 
