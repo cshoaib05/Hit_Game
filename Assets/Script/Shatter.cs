9cf10d5981b5ce1b2ff4b5b33418f07bca93967c
cshoaib05 <choudharyshoaib05@gmail.com>
2019-08-17 00:52:25 +0530

    shatter done

--- Assets/Script/Shatter.cs
+++ Assets/Script/Shatter.cs
@@ -11,17 +11,28 @@ public class Shatter : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Balloon"))
         {
-            gameObject.SetActive(false);
-            Instantiate(shatter, transform.position, transform.rotation);
-            StartCoroutine(destroyobj());
+            collision.gameObject.SetActive(false);
+            shatter = Instantiate(Ballon, transform.position, transform.rotation);
+            Destroy(shatter, 2);
         }
+
+        if (collision.gameObject.CompareTag("jelly"))
+        {
+            collision.gameObject.SetActive(false);
+            shatter = Instantiate(Jelly, transform.position, transform.rotation);
+            Destroy(shatter, 2);
+        }
+        if (collision.gameObject.CompareTag("bottle"))
+        {
+            collision.gameObject.SetActive(false);
+            shatter= Instantiate(Bottle, transform.position, transform.rotation);
+            Destroy(shatter, 2);
+        }
+
     }
 
-    IEnumerator destroyobj()
-    {
-        
-        yield return new WaitForSeconds(2);
-    }
+     
+
 }
